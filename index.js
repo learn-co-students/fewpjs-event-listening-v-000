cdb70f19267ebc17c37b4a208e47f2039b32d2d2
dani8439 <dani8439@aol.com>
2019-10-15 12:42:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+// const input = document.getElementById('input');
+const input = document.querySelector('input');
+
+
 function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    // alert('I was clicked!');
+    return "clicked"
+  });
 }
