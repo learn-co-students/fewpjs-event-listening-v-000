4600552c5e1858635d256d8fe5d4c3c4b814cf3e
Gingertonic <bethmschofield@gmail.com>
2019-11-26 15:44:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function addingEventListener() {
     const input = document.getElementById('input');
-    input.addEventListener('click', alert('Ouch!'));
+    input.addEventListener('click', ouch);
 }
+
+function ouch(){
+    console.log('Ouch');
+}
+
+addingEventListener();
+
+
