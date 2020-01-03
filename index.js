42d41ace5c1aa4e1f89802fec6853cebb94e5d7c
John Blank <veloblank@gmail.com>
2020-01-03 05:19:44 -0500

    Practice. Refactor arrow function.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 function addingEventListener() {
-  const input = document.getElementById('input');
-  input.addEventListener('click', function (event) {
-    alert('I was clicked!');
-  });
+  const input = document.getElementById("input");
+  input.addEventListener("click", () => alert("I was clicked!"));
 }
