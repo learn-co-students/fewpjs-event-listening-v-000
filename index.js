307cbc534397f45f8ebd5571df8cbfb6c012ab25
eryzna <eryzna@gmail.com>
2020-07-03 21:00:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const input = document.querySelector('input');
-
 function addingEventListener() {
     const input = document.getElementById('input');
     input.addEventListener('click', function(event) {
